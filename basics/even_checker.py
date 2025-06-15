b8acce01795e97886ff1dafecfdfd92723e45bc0
thealeskerov <161497084+thealeskerov@users.noreply.github.com>
2025-06-15 21:53:16 +0200

    Update even_checker.py

--- basics/even_checker.py
+++ basics/even_checker.py
@@ -1,7 +1,9 @@
-# This program checks if a number is even or odd
+# Check if a number is even or odd
 
+# Ask user for a number
 number = int(input("Enter a number: "))
 
+# Use modulo to check for evenness
 if number % 2 == 0:
     print("The number is even.")
 else:

5cdaa3766628605deb2d9fb84af3d94e1a96e817
thealeskerov <161497084+thealeskerov@users.noreply.github.com>
2025-06-15 21:53:35 +0200

    Update multiplication_table.py

--- multiplication_table.py
+++ multiplication_table.py
@@ -1,6 +1,8 @@
-# This program prints the multiplication table of a number
+# Print the multiplication table for a number
 
-num = int(input("Enter a number to see its multiplication table: "))
+# Get the number from user
+num = int(input("Enter a number: "))
 
+# Loop from 1 to 10 and multiply
 for i in range(1, 11):
     print(f"{num} x {i} = {num * i}")

2bfce7022cff362393616b5f273dd4c5688be785
thealeskerov <161497084+thealeskerov@users.noreply.github.com>
2025-06-15 21:54:01 +0200

    Update guess_number.py

--- games/guess_number.py
+++ games/guess_number.py
@@ -1,10 +1,14 @@
-# Guess the number game
+# Simple number guessing game
 
-import random
+import random  # To generate a random number
 
+# Generate a random number between 1 and 10
 secret = random.randint(1, 10)
+
+# Ask user to guess
 guess = int(input("Guess a number between 1 and 10: "))
 
+# Check if user guessed correctly
 if guess == secret:
     print("🎉 You guessed it!")
 else:
